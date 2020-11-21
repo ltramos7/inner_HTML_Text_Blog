8d3e37db1965eb700629d917779470b8e7338624
Linda <ltramos7@gmail.com>
2020-11-21 12:02:41 -0600

    JS functions logging innerHTML, innerText, textContent.

---
+++ index.js
@@ -0,0 +1,15 @@
+showInnerHTML = () => {
+    const exampleElement = document.getElementById("example")
+    console.log(exampleElement.innerHTML)
+}
+
+showInnerText = () => {
+    const exampleElement = document.getElementById("example")
+    console.log(exampleElement.innerText)
+}
+
+showTextContent = () => {
+    const exampleElement = document.getElementById("example")
+    console.log(exampleElement.textContent)
+}
+
